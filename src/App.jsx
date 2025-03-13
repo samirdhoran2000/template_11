1bf90cf8a7640d617d97672ee4a48042a002a76f
SAMIR DHORAN <samirdhoran2000@gmail.com>
2025-03-13 11:27:59 +0530

    optimise seo details

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import HomePage from "./pages/HomePage";
 import BlogContent from "./components/BlogContent";
 import Footer from "./components/Footer";
 import FloatingButtons from "./components/FloatingButtons";
+import { useSeoData } from "./hooks/useSeoData";
+import { Loader } from "lucide-react";
 
 function App() {
+
+    const { seoData, loading, error } = useSeoData();
+
+    if (loading) {
+      return  (
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <div className="text-center">
+          <Loader
+            size={40}
+            className="text-purple-400 animate-spin mx-auto mb-4"
+          />
+          <p className="text-gray-300">Loading...</p>
+        </div>
+      </div>
+    );
+    }
+
+    if (error) {
+      console.log("Error loading SEO data:", error);
+      // Continue rendering the app even if SEO data fails to load
+    }
   return (
     <>
       <Router>
