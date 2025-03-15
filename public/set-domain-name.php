0fe06447b2c25d64d4c505f13d731fe0619c4a71
raj-umakant-patil <techbelievers@gmail.com>
2025-03-15 23:15:43 +0530

    fix

--- public/set-domain-name.php
+++ public/set-domain-name.php
@@ -1,10 +1,9 @@
 <?php
 // Get the domain from the request
-$domain = $_SERVER['HTTP_HOST'];
+function setDomain() {
+    return $_SERVER['HTTP_HOST'];
+}
 
-// Set the environment variable
-putenv("VITE_SLUG_URL=$domain");
-
-// Verify if it is set
-echo "VITE_SLUG_URL: " . getenv("VITE_SLUG_URL");
+// Use the function
+echo "Domain: " . setDomain();
 ?>
