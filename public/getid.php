f2d3587308378c42960b7ea566951e6b521c1887
raj-umakant-patil <techbelievers@gmail.com>
2025-03-16 02:08:38 +0530

    fix

---
+++ public/getid.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+
+function getTemplateId($domain) {
+    $apiUrl = "https://www.buyindiahomes.in/api/template?website=" . urlencode($domain);
+
+    // Fetch template ID using cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$response) {
+        return null; // Return null if API call fails
+    }
+
+    $data = json_decode($response, true);
+    return $data['templateId'] ?? null;
+}
+
+// If called directly, return JSON response
+if (isset($_GET['domain'])) {
+    $templateId = getTemplateId($_GET['domain']);
+    echo json_encode(["templateId" => $templateId]);
+}
+?>
