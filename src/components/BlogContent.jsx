4ae0c081820bacba4422502cf1f10977730ee3d4
raj-umakant-patil <techbelievers@gmail.com>
2025-05-20 00:05:01 +0530

    fix

--- src/components/BlogContent.jsx
+++ src/components/BlogContent.jsx
@@ -177,7 +177,7 @@ const BlogContent = () => {
                 transition: "max-height 0.5s ease",
               }}
             >
-              <div dangerouslySetInnerHTML={{ __html: blog.post_content }} />
+              <div className="blog-content" dangerouslySetInnerHTML={{ __html: blog.post_content }} />
             </div>
 
             {/* Gradient overlay when collapsed */}
